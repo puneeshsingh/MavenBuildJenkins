3f962f11342a43e5fb0a7b1f2fdab1f1ab80ca86
Puneesh Singh <puneesh.singh@gmail.com>
2019-05-31 22:07:38 -0400

    This project will be build using Jenkins.

---
+++ MavenBuildJenkins/src/test/java/AppTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Setup");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("Tear Down");
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
